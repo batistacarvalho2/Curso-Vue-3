b833387cf83377e41df336e89c6d4b63ca4ad885
João <j.batistacarvalho95@gmail.com>
2025-05-29 22:17:33 -0300

    fix: finalizando lista de tarefa

--- vue-3-todo/src/components/ListTasks.vue
+++ vue-3-todo/src/components/ListTasks.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <v-text-field label="Add Task"></v-text-field>
+    <v-text-field
+    clearacle
+     label="Add Task"
+     v-model="task.title"
+     @keyup.enter="addTask"
+     ></v-text-field>
 
     <v-list lines="three" select-strategy="classic">
       <v-list-subheader>General</v-list-subheader>
 
-      <v-list-item value="notifications">
+      <v-list-item 
+      v-for="task, index in tasks"
+      :key="index"
+      :value="index"
+      >
         <template v-slot:prepend="{ isActive }">
           <v-list-item-action start>
             <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
             </v-list-item-action>
         </template>
 
-      <v-list-item-title>notifications</v-list-item-title>
+      <v-list-item-title>{{task.title}}</v-list-item-title>
 
       <v-list-item-subtitle>
-        Notify me About
+        {{ task.description }}
       </v-list-item-subtitle>
       </v-list-item>
     </v-list>
@@ -23,11 +32,33 @@
 </template>
 
 <script setup>
-const tasks = [
+import {ref} from 'vue';
+
+const tasks = ref ([
   {
     title: "Estudar Vue",
     description: "Estudar vue com Vuetify"
+  },
+  {
+    title: "Ler a documentação",
+    description: "Ler a documentação com Vuetify"
   }
-]
+]);
+
+const task = ref({
+  title:"",
+  description:""
+});
+const addTask = ()=>{
+      tasks.value.push({
+        title : task.value.title,
+        description : task.value.description
+      });
+    task.value = {
+    title:"",
+    description:""
+    }
+}
+
 </script>
 
