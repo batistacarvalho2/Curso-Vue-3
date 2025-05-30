0545fd5caa171748fd755d7662c369f260f08c3b
João <j.batistacarvalho95@gmail.com>
2025-05-29 21:22:57 -0300

    alteração page para checkbox

--- vue-3-todo/src/components/ListTasks.vue
+++ vue-3-todo/src/components/ListTasks.vue
@@ -1,41 +1,33 @@
 <template>
   <div>
-    <v-list lines="three">
-      <v-list-subheader>User Controls</v-list-subheader>
-      <v-list-item
-        v-for="item in userControls"
-        :key="item.value"
-        :subtitle="item.subtitle"
-        :title="item.title"
-      ></v-list-item>
-    </v-list>
+    <v-text-field label="Add Task"></v-text-field>
 
-    <v-divider></v-divider>
-
-    <v-list
-      v-model:selected="settingsSelection"
-      lines="three"
-      select-strategy="leaf"
-    >
+    <v-list lines="three" select-strategy="classic">
       <v-list-subheader>General</v-list-subheader>
-      <v-list-item
-        v-for="item in settingsItems"
-        :key="item.value"
-        :subtitle="item.subtitle"
-        :title="item.title"
-        :value="item.value"
-      >
-        <template v-slot:prepend="{ isSelected, select }">
+
+      <v-list-item value="notifications">
+        <template v-slot:prepend="{ isActive }">
           <v-list-item-action start>
-            <v-checkbox-btn :model-value="isSelected" @update:model-value="select"></v-checkbox-btn>
-          </v-list-item-action>
+            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
+            </v-list-item-action>
         </template>
+
+      <v-list-item-title>notifications</v-list-item-title>
+
+      <v-list-item-subtitle>
+        Notify me About
+      </v-list-item-subtitle>
       </v-list-item>
     </v-list>
   </div>
 </template>
 
-<script>
-
+<script setup>
+const tasks = [
+  {
+    title: "Estudar Vue",
+    description: "Estudar vue com Vuetify"
+  }
+]
 </script>
 
